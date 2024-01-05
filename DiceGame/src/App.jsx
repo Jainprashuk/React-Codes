0d28fafff4ceed27c5e853f5881090a0ec8f81f4
prashuk jain <29jainprashuk@gmail.com>
2024-01-05 23:03:36 +0530

    start dise game

---
+++ DiceGame/src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10 px;
+`;
+
+function App() {
+  return (
+    <>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
